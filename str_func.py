b9f244d23f1d801c4591d2b1c2e0c67ba276b5dd
Aleksandr Shibanov <shadow.sanya28@gmail.com>
2023-07-20 19:47:27 +0500

    add foo2

--- str_func.py
+++ str_func.py
@@ -1,6 +1,13 @@
 def foo(text):
+    """большие буквы"""
     return text.upper()
 
+def foo2(text):
+    '''новое слово с большой буквы'''
+    return text.title()
+
 user_input = input('Введите текст')
+user_input_2 = input('Введите текст')
 
 print(foo(user_input))
+print(foo2(user_input_2))
